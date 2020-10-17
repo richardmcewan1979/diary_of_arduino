50e29469c30d38a5050412bba62bfea76d9e297a
richardmcewan1979 <richmcewan@hotmail.com>
2020-10-17 17:05:36 +0100

    Add files via upload

---
+++ distance_sensor_2020_test1/distance_sensor_2020_test1.ino
@@ -0,0 +1,55 @@
+const unsigned int TRIG_PIN=13;
+const unsigned int ECHO_PIN=12;
+const unsigned int BAUD_RATE=9600;
+
+void setup() {
+  //for distance sensor enable
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  Serial.begin(BAUD_RATE);
+
+  //for lights enable
+  pinMode(LED_BUILTIN, OUTPUT);
+  //pinMode(11, OUTPUT);
+}
+
+void loop() {
+
+
+  //Lights blinking
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+
+/*
+  digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(11, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+
+*/
+
+
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  
+// distance sesnor logic
+ const unsigned long duration= pulseIn(ECHO_PIN, HIGH);
+ int distance= duration/29/2;
+ if(duration==0){
+   Serial.println("Warning: no pulse from sensor");
+   } 
+  else{
+      Serial.print("distance to nearest object:");
+      Serial.println(distance);
+      Serial.println(" cm");
+      //This is should send to the processing sketch
+      //may need a handshaking element to be added
+      Serial.write(distance);
+  }
+ delay(100);
+ }
